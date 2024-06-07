fe09eae21915c562bb99c4c24dbe281cb805b95c
IhnatVelykoivan <ihnat.velykoivan@gmail.com>
2024-06-07 15:56:39 +0300

    #star gamedev(some game) with 'goIT' marathon (top, left)

--- .venv/main.py
+++ .venv/main.py
@@ -22,7 +22,7 @@ player_speed = [1, 1]
 playing = True
 
 while playing:
-    FPS.tick(500)
+    FPS.tick(300)
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -30,12 +30,17 @@ while playing:
     main_display.fill(COLOR_BLACK)
 
     if player_rect.bottom >= HEIGHT:
-        player_speed[1] = -player_speed[1]
+        player_speed[-1] = -player_speed[1]
 
     if player_rect.right >= WIDTH:
         player_speed[0] = -player_speed[0]
 
-    print(player_rect.bottom)
+    if player_rect.top < 0:
+        player_speed[1] = -player_speed[1]
+
+    if player_rect.left < 0:
+        player_speed[0] = -player_speed[0]
+
 
     main_display.blit(player, player_rect)
 
